a42b1cc95393d55b9c66cb89d09d087d417f6880
Martin Suarez-Viacava <kontrolakka@gmail.com>
2018-08-26 17:05:58 -0400

    Initial Translate Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,21 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private titleService: Title) {
-    titleService.setTitle('Pini Rental');
+  constructor(public translate: TranslateService,
+              private titleService: Title) {
+    // titleService.setTitle('Pini Rental');
+
+    // Start Translations Service
+    this.configureTranslations();
+  }
+
+  private configureTranslations() {
+    const defaultLanguage = 'es';  // TODO: Llevar a un archivo de configuracion.
+    this.translate.addLangs(['en', 'es']);
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang(defaultLanguage);
+
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang.match(/en|es/) ? browserLang : defaultLanguage);
   }
 }
